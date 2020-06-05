34b2ddc374af3f82f6da54a17793f91ab7a360eb
Nick Stricker <strickerdev@gmail.com>
2020-06-04 22:08:01 -0500

    sorting notes

--- tl-lecture/recursive_complexity.py
+++ tl-lecture/recursive_complexity.py
@@ -1,14 +1,28 @@
-totalTimes = 0
-
+total_times_count = 0
+​
 def count_down(n):
-    global totalTimes
-    totalTimes += 1
-
+    global total_times_count
+    total_times_count += 1
+​
     if n == 0:
         return
-
-    count_down(n-1)
-
-
+    
+    count_down(n - 1)
+    count_down(n - 1)
+​
+def half(n):
+    if n <= 1:
+        return
+    
+    half(n / 2)
+​
+# O(log n)
+​
+# O(c^n)
+# O(n)
+​
+#(2^5)
+# 2^10
+​
 count_down(5)
-print(totalTimes)
+print(total_times_count)
